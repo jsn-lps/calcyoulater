7291b1eab3cdf597ea4690537e78a027e18c1c0a
jsn-lps <lpsjsn94@gmail.com>
2022-09-01 14:45:11 -0400

    css and framework

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="Page">
+      <div className="CalcBody">
+        <div id="display">69420</div>
+        <div id="button-area">
+          <button className="Button" id="divide">/</button>
+          <button className="Button" id="multiply">x</button>
+          <button className="Button" id="subtract">-</button>
+          <button className="Button" id="clear">clear</button>
+          <button className="Button" id="seven">7</button>
+          <button className="Button" id="eight">8</button>
+          <button className="Button" id="nine">9</button>
+          <button className="Button" id="add">+</button>
+          <button className="Button" id="four">4</button>
+          <button className="Button" id="five">5</button>
+          <button className="Button" id="six">6</button>
+          <button className="Button" id="equals">=</button>
+          <button className="Button" id="one">1</button>
+          <button className="Button" id="two">2</button>
+          <button className="Button" id="three">3</button>
+          <button className="Button" id="smiley">:)</button>
+          <button className="Button" id="zero">0</button>
+          <button className="Button" id="decimal">.</button>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 
 export default App;
