72ba8ae631a41b9c610b65d459eae6439df90ef2
jsn-lps <lpsjsn94@gmail.com>
2022-09-09 17:12:17 -0400

    working calculator (note: minus isnt negative)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import React from 'react';
 
 const DisplayContext = createContext({});
 let clearDisplay = false; // toggle for display
-
+let runTotal = null;
+let numerator = null;
+let operator = null;
+let afterEquals = false;
 
 const Display = () => {
   const { display } = useContext(DisplayContext);
@@ -16,23 +19,22 @@ const Display = () => {
 }
 
 const NumPad = () => {
-  const { display, operator, setOperator, setDisplay, prevNum, setPrevNum } = useContext(DisplayContext);  console.log(display)
+  const { display, setDisplay, prevNum, setPrevNum, operand, setOperand } = useContext(DisplayContext);  
+  // console.log(display)
   
   const btnClicked = (e) => {
-
+    
     HandleButton(e, 
       display,
       setDisplay,
-      operator,
-      setOperator,
-      prevNum,
-      setPrevNum);}
-
-  return (
-    <div id="button-area">
+      operand, 
+      setOperand);}
+      
+      return (
+        <div id="button-area">
+    <button onClick={btnClicked} className="Button" id="neg" value={"neg"}>neg</button>
     <button onClick={btnClicked} className="Button" id="divide" value={"/"}>/</button>
     <button onClick={btnClicked} className="Button" id="multiply" value={"x"}>x</button>
-    <button onClick={btnClicked} className="Button" id="subtract" value={"-"}>-</button>
     <button onClick={btnClicked} className="Button" id="clear" value="clear">clear</button>
     <button onClick={btnClicked} className="Button" id="seven" value={7}>7</button>
     <button onClick={btnClicked} className="Button" id="eight" value={8}>8</button>
@@ -41,6 +43,7 @@ const NumPad = () => {
     <button onClick={btnClicked} className="Button" id="four" value={4}>4</button>
     <button onClick={btnClicked} className="Button" id="five" value={5}>5</button>
     <button onClick={btnClicked} className="Button" id="six" value={6}>6</button>
+    <button onClick={btnClicked} className="Button" id="subtract" value={"-"}>-</button>
     <button onClick={btnClicked} className="Button" id="equals" value={"="}>=</button>
     <button onClick={btnClicked} className="Button" id="one" value={1}>1</button>
     <button onClick={btnClicked} className="Button" id="two" value={2}>2</button>
@@ -52,86 +55,100 @@ const NumPad = () => {
   )
 }
 
+const DoMath = (display, setDisplay) => {
+  clearDisplay = true;
+  numerator = parseFloat(display);
 
-const HandleButton = (e, display, setDisplay, operator, setOperator, prevNum, setPrevNum) => {
+  switch(true) {
+    case (operator === "+"):
+      runTotal = runTotal + numerator;
+      break;
+      
+    case (operator === "-"):
+      runTotal = runTotal - numerator;
+      break;
+
+    case (operator === "x"):
+      runTotal = runTotal * numerator;
+      break;
+
+    case (operator === "/"):
+      runTotal = runTotal / numerator;
+      break;
+    }
+    setDisplay(runTotal);
+
+}
+
+const HandleButton = (e, display, setDisplay, operand, setOperand, prevNum, setPrevNum) => {
   let btnInput = e.target.value;
-
-  // console.log(clearDisplay)
-
+  
   // clear button
   if (btnInput === "clear") {
     setDisplay(0);
-    setOperator(null);
-    setPrevNum(null);
+    operator = null;
+    runTotal = null;
+    operand =  null;
+    numerator = null;
     clearDisplay = false;
+    afterEquals = false;
 
     // number input handling
   } else if (parseInt(btnInput) || btnInput == 0 || btnInput == ".") {
-    
       if (display == 0 || clearDisplay) { // if display == 0 set display to number input
+
         setDisplay(btnInput);
         clearDisplay = false;
+
       } else {
-        if (btnInput == "." && display % 1 != 0){ // NO REPEATED DECIMALS
-          return
+
+        if (btnInput == "."){
+          if (display % 1 != 0 || display[(display.toString().length - 1)] == "."){
+            return
+          }
         }
         setDisplay(display + btnInput);
       }
-      
-      // operator handling
-  } else if (btnInput === "+") {
-    setOperator("+");
-    setPrevNum(display)
-    clearDisplay = true;
 
-  } else if (btnInput === "-") {
-    setOperator("-");
-    setPrevNum(display)
-    clearDisplay = true;
+  } else if (btnInput === "+" || btnInput === "-" || btnInput === "x" || btnInput === "/") {
+    if (afterEquals == true) {
+      afterEquals = false;
+      runTotal = parseFloat(display);
+      operator = btnInput;
+      clearDisplay = true;
+    } else {
+    if (runTotal == null) {
+      runTotal = parseFloat(display);
+      clearDisplay = true;
+      operator = btnInput;
 
-  } else if (btnInput === "x") {
-    setOperator("x");
-    setPrevNum(display)
-    clearDisplay = true;
-    
-  } else if (btnInput === "/") {
-    setOperator("/");
-    setPrevNum(display)
-    clearDisplay = true;  
+    } else {
+      DoMath(parseFloat(display), setDisplay)
 
-    // equals handling
-  } else if (btnInput === "=") {
-    switch(true) {
-      case (operator === "+"):
-        setDisplay(parseFloat(prevNum) + parseFloat(display));
-        break;
-      case (operator === "-"):
-        setDisplay(parseFloat(prevNum) - parseFloat(display));
-        break;
-      case (operator === "x"):
-        setDisplay(parseFloat(prevNum) * parseFloat(display));
-        break;
-      case (operator === "/"):
-        setDisplay(parseFloat(prevNum) / parseFloat(display));
-        break;
-      }
+      operator = btnInput;
     }
   }
 
-
-
+  } else if (btnInput === "=") {
+    DoMath(parseFloat(display), setDisplay);
+    afterEquals = true;
+    }
+    console.log("run total " + runTotal);
+    console.log("operator " + operator);
+    console.log("numerator " + numerator);
+    console.log("afterEquals is " + afterEquals)
+  }
 
 ///////////////////////////////////////////
 
 function App() {
   const [display, setDisplay] = useState(0);
-  const [operator, setOperator] = useState(null);
-  const [prevNum, setPrevNum] = useState(null);
+  const [operand, setOperand] = useState(null);
 
   return (
     <div className="Page">
       <div className="CalcBody">
-        <DisplayContext.Provider value={{display, setDisplay, operator, setOperator, prevNum, setPrevNum}}>
+        <DisplayContext.Provider value={{display, setDisplay, operand, setOperand}}>
           <Display />
           <NumPad />
         </DisplayContext.Provider>
@@ -139,53 +156,6 @@ function App() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default App;
